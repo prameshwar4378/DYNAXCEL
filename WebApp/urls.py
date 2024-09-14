5fe898765030f8cfdcf04130b51a3745f7f6f3ad
Rameshwar Pawar <prameshwar4378@gmail.com>
2024-09-14 08:25:06 +0530

    14-09-2024

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -4,4 +4,5 @@ from WebApp.views import  *
 urlpatterns = [
     path('/contact-us', contact_us, name="website_contact_us"),
     path('/about_us', about_us, name="website_about_us"),
+    path('/web_news', web_news, name="web_news"),
 ]
--- WebApp/views.py
+++ WebApp/views.py
@@ -10,3 +10,6 @@ def contact_us(request):
 
 def about_us(request):
     return render(request,"about_us.html")
+
+def web_news(request):
+    return render(request,"web_news.html")
