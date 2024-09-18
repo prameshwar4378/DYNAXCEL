d5bf8b3648d18c7caac296374bc8c3afff1fa15f
krishna thore <krushnathore111@gmail.com>
2024-09-18 22:50:47 +0530

    added services pages - Krishna - 18/09

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -12,4 +12,13 @@ urlpatterns = [
     path('/career_apply_job', career_apply_job, name="career_apply_job"),
     path('/sustainability', sustainability, name="sustainability"),
 
+    path('/services_stainless_steel', stainless_steel, name="stainless_steel"),
+    path('/services_ASME_PED_Vessels', services_ASME_PED_Vessels, name="services_ASME_PED_Vessels"),
+    path('/services_railway_metro', services_railway_metro, name="services_railway_metro"),
+    path('/services_food_farma', services_food_farma, name="services_food_farma"),
+    path('/services_psa_based', services_psa_based, name="services_psa_based"),
+    path('/services_defence_equipements', services_defence_equipements, name="services_defence_equipements"),
+
+
+
 ]
--- WebApp/views.py
+++ WebApp/views.py
@@ -31,4 +31,20 @@ def career_apply_job(request):
 def sustainability(request):
     return render(request,"sustainability.html")
 
+def stainless_steel(request):
+    return render(request,"services_stainless_steel.html")
 
+def services_ASME_PED_Vessels(request):
+    return render(request,"services_ASME_PED_Vessels.html")
+
+def services_food_farma(request):
+    return render(request,"services_food_farma.html")
+
+def services_railway_metro(request):
+    return render(request,"services_railway_metro.html")
+
+def services_psa_based(request):
+    return render(request,"services_psa_based.html")
+
+def services_defence_equipements(request):
+    return render(request,"services_defence_equipements.html")
